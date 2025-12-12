60a8db56fb572b72fdaa3055acc0be410b59727e
ahlamad <ahlam.adda@hva.nl>
2025-12-12 11:23:26 +0100

    #9 svg color nesting change

---
+++ settings.js
@@ -0,0 +1,8 @@
+let showMoreButton = document.querySelector('.only-text-button');
+
+let doSomething = function() {
+    alert('Het werkt!');
+}
+
+showMoreButton.addEventListener('click', doSomething);
+
